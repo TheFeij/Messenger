466a4a4b88230a70ef09b6d656122c4d3b7d58dc
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-02-03 20:06:29 +0330

    changed type of id from uint to uint64

--- db/models/contact.go
+++ db/models/contact.go
@@ -1,7 +1,7 @@
 package models
 
 type Contact struct {
-	UserID      uint   `gorm:"column:user_id"`
-	ContactID   uint   `gorm:"column:contact_id"`
+	UserID      uint64 `gorm:"column:user_id"`
+	ContactID   uint64 `gorm:"column:contact_id"`
 	ContactName string `gorm:"column:contact_name"`
 }
